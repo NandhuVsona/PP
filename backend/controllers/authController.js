0bf31b27646731c59b99dc8cf977813762774b27
Learn Something <131090906+NandhuVsona@users.noreply.github.com>
2024-11-02 20:00:39 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -30,7 +30,7 @@ const createSendToken = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    secure: process.env.JWT_COOKIE_SECURE,
+    secure: true,
     HttpOnly: true,
     sameSite: "none",
   };
